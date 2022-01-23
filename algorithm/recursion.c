569070a9bfc8c2e2e2b2784bc0c6cb44911f4fca
Arrow <1512803831@qq.com>
2022-01-23 23:23:03 +0800

    file changed

--- algorithm/recursion.c
+++ algorithm/recursion.c
@@ -1,36 +1,28 @@
 # include <stdio.h>
 
 
-/************* 求数组前n项中的最大值 *******
-
-max(arr,n)=max(max(arr,n-1),arr[n])(递归关系)
-sum(arr,0)=arr[0] (出口)
-
-*******************************************/
-
-int arrsum(int arr[],int n){
-	if(n==0)
-		return arr[0];
+/************* 求数组前n项中的最大值 *******/
+int maxarr(int arr[],int n){
+	if(n==0)							//递归出口
+		return arr[0];					
+	else if (maxarr(arr,n-1)>arr[n])	//递归关系
+		return maxarr(arr,n-1);
 	else
-		return arrsum(arr,n-1)+arr[n];
+		return arr[n];
 }
 
-
-
-//sum=1+2+3+...+100
+/************* sum=1+2+3+...+100 *******/
 int sum(int n){
 	if (n==1){
 		return 1;
 	}
-	else 
+	else
 		return sum(n-1)+n;
 }
 
 /************ 求数组前n项和 *************
-
 sum(arr,n)=sum(arr,n-1)+arr[n] (递归关系)
 sum(arr,0)=arr[0] (出口)
-
 ****************************************/
 int arrsum(int arr[],int n){
 	if(n==0)
@@ -45,7 +37,10 @@ int main(){
 	int s = sum(100);
 	printf("sum = %d\n",s);
 
-	int arr[9]={9,8,7,6,5,4,3,2,1};
+	int arr[9]={1,0,7,6,5,4,3,2,1};
 	int as=arrsum(arr,8);
 	printf("arrsum = %d\n",as);
+
+	int xa=maxarr(arr,8);
+	printf("maxarr = %d\n",xa);
 }
