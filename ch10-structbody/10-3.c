967279ca72584cfb2330ad4367c2e01a1c27a950
Arrow <1512803831@qq.com>
2022-02-15 15:52:58 +0800

    ch10

--- ch10-structbody/10-3.c
+++ ch10-structbody/10-3.c
@@ -4,6 +4,23 @@
  *结构体数组
  * */
 
+struct pay_rec{
+	long id;
+	char name[20];
+	float pay;
+};
+
 int main(){
 	int i;
+	struct pay_rec employee[5]={
+		{1,"李小龙",655.1},
+		{2,"李小龙2",655.1},
+		{3,"李小龙3",655.1},
+		{4,"李小龙4",655.1},
+		{5,"李小龙5",655.1}
+	};
+	for (i=0;i<5;++i)
+		printf("%-20ld%-20s%-20.2f\n",
+				employee[i].id, employee[i].name, employee[i].pay);
+	return 0;
 }
---
+++ ch10-structbody/10-4.c
@@ -0,0 +1,14 @@
+/*
+ *结构体的传递
+ * 
+ * */
+
+struct employee{
+	int id_num;
+	double pay_rate;
+	double hours;
+};
+
+int main(){
+	struct employee emp={}
+}
